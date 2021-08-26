f0a4a166e05541ab9aa3bf539202d5285666f259
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-26 15:38:42 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,46 @@
+#str
+a="hi Krishi"
+print (a)
+#NUMERICS 
+#int,float,complex
+b=30
+c=39.8
+d=b+c
+print (b,c,d)
+e=39j
+print(e)
+#SEQUENCE- lists tuples range
+f=["BENZ","AUDI","BMW"]
+g=("BENZ","AUDI","BMW")
+h=range(26)
+print (f,g,h)
+#MAPPING-set frozen set dict
+i={"BMW","AUDI","BENZ"}
+j=({"BMW","AUDI","BENZ"})
+k={"name":"KRISHI","AGE":23}
+print(i,j,k)
+#BOOLEAN
+l=True
+print (l)
+#BINARY TYPES- byte,bytearray,memoryview
+m=b"KRISHI"
+print(m)
+n=bytearray(2)
+print(n)
+o=memoryview(bytes(n))
+print(o)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
+print(type(o))
