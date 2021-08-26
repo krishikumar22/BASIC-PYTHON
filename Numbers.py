c52de08cf128ee076314e50b266a18302185e429
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-26 15:38:58 +0530

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,16 @@
+a=47.9e40
+b=34
+c=-2343323823828372
+d=(a+b+c)
+print (a,b,c,d)
+e=20-4j
+f=378-1j
+print(e,f)
+g=float(a)
+print (g)
+i=complex(e)
+print (i)
+j=complex(c)
+print(j)
+import random
+print(random.randrange(1,10))
